bfdbb9b617f0edb9e4a83840a63f93a958ba3b98
Kungerberglund <62596212+Kungerberglund@users.noreply.github.com>
2021-05-31 11:25:13 +0200

    småfix
    Co-Authored-By: null <EmelieKul@users.noreply.github.com>
    Co-Authored-By: null <moasvennesson@users.noreply.github.com>
    Co-Authored-By: null <Kungerberglund@users.noreply.github.com>

--- src/ActivityHandler.js
+++ src/ActivityHandler.js
@@ -170,11 +170,13 @@ export default function ActivityHandler() {
                 <select ref={PeopleRef} type="text" className="form-control">
                     <option value="">Select number of participants...</option>
                     <option value="">Random</option>
-                    <option value="1" >1</option>
+                    <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
                     <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
                     <option value="8">8</option>
                 </select>
                 <b>Price range</b>
